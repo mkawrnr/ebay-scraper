788f4c9b6ad434b5dc9c69d531de3870e32a489d
zedprob <mkawr20@gmail.com>
2022-10-12 14:51:34 +0200

    update requirements.txt

--- eka_tool.py
+++ eka_tool.py
@@ -1,7 +1,6 @@
 import sys
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
