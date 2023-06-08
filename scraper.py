1a833fe4c069ca81dda63ee046e7bd11b865a2fd
mkawrnr <info@petimuscredimus.de>
2023-06-08 20:48:20 +0200

    commit

--- scraper.py
+++ scraper.py
@@ -27,26 +27,11 @@ args = parser.parse_args()
 FILTER_WORDS = [
     'kaputt',
     'bastler',
-<<<<<<< HEAD
     'tausche',
     'defekt',
     'risse',
     'hülle',
     'case'
-=======
-    'repair',
-    'Displayschaden',
-    'displayschaden',
-    'schaden',
-    'risse',
-    'Risse',
-    'riss',
-    'Schaden',
-    'Tausche',
-    'Tauschen',
-    'Tausch',
-    'tausch'
->>>>>>> refs/remotes/origin/main
 ]
 
 
